0f92ab79c46b343c7f696cbab3359693c9d35428
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-08-02 15:59:56 -0400

    Resto 2 - 1075

---
+++ src/Desafio1075/Main.java
@@ -0,0 +1,22 @@
+package Desafio1075;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		int valorN = sc.nextInt();
+		
+		for(int i = 1; i < 10000; i++) {
+			if(i%valorN == 2) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+	}
+}
